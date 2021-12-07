673bfda0dad1e60cac3c8e0bbf39d97b6d818c4a
misha1204 <Mikheil.kvashali@gmail.com>
2021-12-07 17:13:44 +0400

    Inheritance

---
+++ OOP With JavaScript/Inheritance/inheritanceConstructorFunctions.js
@@ -0,0 +1,27 @@
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+}
+
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+}
+
+const Student = function(firstName, birthYear, course) {
+    Person.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+// Linking Prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}.`);
+}
+
+const mike = new Student("Mike", 2020, "Computer Science");
+console.log(mike)
+mike.introduce();
+mike.calcAge();
